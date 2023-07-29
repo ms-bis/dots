f74346af389577b9d1e5e90cfba224769a9c3ae0
msbis <nargis7783@gmail.com>
2023-07-29 22:43:43 +0600

    all changed

--- nvim/Nvchad/custom/plugins.lua
+++ nvim/Nvchad/custom/plugins.lua
@@ -1,106 +1,106 @@
 local plugins = {
-  {
-    "sitiom/nvim-numbertoggle",
-    lazy = false,
-  },
-  {
-    "stevearc/dressing.nvim",
-    lazy = false,
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  {
-    "ethanholz/nvim-lastplace",
-    lazy = false,
-    config = function()
-      require("nvim-lastplace").setup {}
-    end,
-  },
-  {
-    "ThePrimeagen/vim-be-good",
-    event = "VeryLazy",
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require "dap"
-      local dapui = require "dapui"
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "mfussenegger/nvim-dap",
-    },
-    opts = {
-      handlers = {},
-      ensure_installed = {
-        "codelldb",
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-dap",
-    config = function(_, _)
-      require("core.utils").load_mappings "dap"
-    end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return require "custom.configs.null-ls"
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "RimuhRimu/runthis.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "clangd",
-        "clang-format",
-        "codelldb",
-      },
-    },
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = function()
-      return require "custom.configs.nvimtree"
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = { "html", "css", "bash", "c", "cpp", "python", "fish", "toml", "lua" },
-    },
-  },
+	{
+		"sitiom/nvim-numbertoggle",
+		lazy = false,
+	},
+	{
+		"stevearc/dressing.nvim",
+		lazy = false,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	{
+		"ethanholz/nvim-lastplace",
+		lazy = false,
+		config = function()
+			require("nvim-lastplace").setup({})
+		end,
+	},
+	{
+		"ThePrimeagen/vim-be-good",
+		event = "VeryLazy",
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		event = "VeryLazy",
+		dependencies = "mfussenegger/nvim-dap",
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		opts = {
+			handlers = {},
+			ensure_installed = {
+				"codelldb",
+			},
+		},
+	},
+	{
+		"mfussenegger/nvim-dap",
+		config = function(_, _)
+			require("core.utils").load_mappings("dap")
+		end,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		event = "VeryLazy",
+		opts = function()
+			return require("custom.configs.null-ls")
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"RimuhRimu/runthis.nvim",
+		event = "VeryLazy",
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"clangd",
+				"clang-format",
+				"codelldb",
+			},
+		},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = function()
+			return require("custom.configs.nvimtree")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = { "html", "css", "bash", "c", "cpp", "python", "fish", "toml", "lua" },
+		},
+	},
 }
 
 return plugins
